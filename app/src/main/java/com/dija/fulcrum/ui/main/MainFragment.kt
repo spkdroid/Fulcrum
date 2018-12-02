854ee43fae3e6b4587bafa9aaa3fc4e0de9d0d42
Ramkumar Velmurugan <vrk1990@outlook.com>
2018-12-02 08:18:22 -0400

    Code Refactored

--- app/src/main/java/com/dija/fulcrum/ui/main/MainFragment.kt
+++ app/src/main/java/com/dija/fulcrum/ui/main/MainFragment.kt
@@ -4,18 +4,13 @@ import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import com.dija.fulcrum.R
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.main_fragment.*
-import java.util.concurrent.TimeUnit
 import com.dija.fulcrum.adapter.AddressAdapter
 
 
@@ -24,8 +19,6 @@ class MainFragment : Fragment() {
     companion object {
         fun newInstance() = MainFragment()
     }
-
-    private var address: ArrayList<String> = ArrayList()
 
     private lateinit var viewModel: MainViewModel
 
@@ -42,45 +35,22 @@ class MainFragment : Fragment() {
         viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
         // TODO: Use the ViewModel
         suggestionList.layoutManager = LinearLayoutManager(context)
-
-        suggestionList.adapter = AddressAdapter(address, requireContext())
+        suggestionList.adapter = AddressAdapter(viewModel.address, requireContext())
 
         autoCompleteTextView?.addTextChangedListener(object : TextWatcher {
-            override fun afterTextChanged(p0: Editable?) {
-
-                if(address.size>0)
-                    address.clear()
-
-                viewModel.placeAutoCompleteAPI.loadPredictions(p0.toString())
-                    .subscribeOn(Schedulers.io())
-                    .debounce(10, TimeUnit.SECONDS)
-                    .observeOn(AndroidSchedulers.mainThread())
-                    .subscribe(
-                        { result ->
-                            run {
-                                val actualList = result!!.getPredictions()
-                                actualList.forEach {
-                                //        prediction_ -> Toast.makeText(context,prediction_!!.description,Toast.LENGTH_SHORT).show()//predictionResult!!.add(prediction_!!.description)
-                                prediction_ -> address.add(prediction_.description)
-                                }
-                                suggestionList.adapter!!.notifyDataSetChanged()
-                            }
-                        },
-                        { error -> Toast.makeText(context, error.message, Toast.LENGTH_SHORT).show() }
-                    )
+            override fun afterTextChanged(searchString: Editable?) {
+                viewModel.clearAddress()
+                viewModel.loadAddressPrediction(searchString.toString(), context!!,suggestionList)
             }
 
-            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            override fun beforeTextChanged(searchString: CharSequence?, p1: Int, p2: Int, p3: Int) {
             }
 
-            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            override fun onTextChanged(searchString: CharSequence?, p1: Int, p2: Int, p3: Int) {
             }
         })
 
-
-
     }
-
 
     }
 
--- app/src/main/java/com/dija/fulcrum/ui/main/MainViewModel.kt
+++ app/src/main/java/com/dija/fulcrum/ui/main/MainViewModel.kt
@@ -1,28 +1,59 @@
 package com.dija.fulcrum.ui.main
 
 import android.arch.lifecycle.ViewModel
+import android.content.Context
 import android.support.v4.app.FragmentActivity
+import android.support.v7.widget.RecyclerView
 import android.widget.Toast
 import com.dija.fulcrum.service.PlaceAutoCompleteAPI
 import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
+import kotlinx.android.synthetic.main.main_fragment.*
 import java.util.concurrent.TimeUnit
 
 class MainViewModel : ViewModel() {
     // TODO: Implement the ViewModel
-    public val placeAutoCompleteAPI by lazy {
+
+     var address: ArrayList<String> = ArrayList()
+
+    val placeAutoCompleteAPI by lazy {
         PlaceAutoCompleteAPI.create()
     }
 
-    companion object {
-        private var predictionResult: ArrayList<String>?=null
+    fun clearAddress(){
+        if(address.size>0)
+            address.clear()
+    }
+
+    fun loadAddressPrediction(search:String,context:Context,suggestionList: RecyclerView) {
+
+        placeAutoCompleteAPI.loadPredictions(search)
+            .subscribeOn(Schedulers.io())
+            .debounce(10, TimeUnit.SECONDS)
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(
+                { result ->
+                    run {
+                        val actualList = result!!.getPredictions()
+                        actualList.forEach {
+                            //        prediction_ -> Toast.makeText(context,prediction_!!.description,Toast.LENGTH_SHORT).show()//predictionResult!!.add(prediction_!!.description)
+                                prediction_ -> address.add(prediction_.description)
+                        }
+                        suggestionList.adapter!!.notifyDataSetChanged()
+                    }
+                },
+                { error -> Toast.makeText(context, error.message, Toast.LENGTH_SHORT).show() }
+            )
     }
 
 
-
-
-
-
-
 }
+
+
+
+
+
+
+
+
